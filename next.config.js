0d103605486e3c4e2ca3ce5103416dd1639cbe77
bob1127 <61307295+bob1127@users.noreply.github.com>
2023-08-23 13:57:57 +0800

    fff

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: ".",
-  images: {
-    loader: "imgix",
-    path: "/",
-  },
+  // assetPrefix: ".",
+ images: {
+        domains: ['www.zensor.com.tw'],
+  //        loader: "imgix",
+  //  path: "/",
+    },
+  // images: {
+  //   loader: "imgix",
+  //   path: "/",
+  //     domains: ['www.zemsor.com.tw'],
+  // },
     output:'export'
 }
 
